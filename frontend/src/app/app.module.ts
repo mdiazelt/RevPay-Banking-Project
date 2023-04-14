8051402e3710ab7981e391a697c719b45d0b28aa
Nick Hodges <nickhodges@Nicks-MBP.home>
2023-04-14 12:55:13 -0400

    personal-account-component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalAccountComponent } from './components/personal-account/personal-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalAccountComponent
   ],
   imports: [
     BrowserModule,
