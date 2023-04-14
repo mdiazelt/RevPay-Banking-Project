c8166d1ff3b7d0058d664f13b7804ff7c8255281
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-04-14 14:46:25 -0500

    added child routes for trans history

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PersonalAccountComponent } from "./components/personal-account/personal-account.component";
+import { PersonalTransactionListComponent } from './components/personal-transaction-list/personal-transaction-list.component';
 
 const routes: Routes = [
-  {path: 'personal', component: PersonalAccountComponent}
+
+  {path: 'personal', component: PersonalAccountComponent},
+
+  {path:'', children:[
+    {path:'personal/:transaction-history', component:PersonalTransactionListComponent}
+    ]},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
