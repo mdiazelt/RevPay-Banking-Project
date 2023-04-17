7168b0be1002d47577ff4106ae4b83c2f75ce6f6
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-04-17 16:13:04 -0500

    added invoice component for the business account

---
+++ frontend/src/app/components/business-invoices/business-invoices.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-business-invoices',
+  templateUrl: './business-invoices.component.html',
+  styleUrls: ['./business-invoices.component.css']
+})
+export class BusinessInvoicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
