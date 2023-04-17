88b2a0d7b1c45e4660ada276838667c3ff4dd28c
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-04-17 13:37:01 -0500

    added submit and reset to the models User

--- frontend/src/app/components/register-account/register-account.component.ts
+++ frontend/src/app/components/register-account/register-account.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from 'src/app/models/user.model';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-register-account',
   templateUrl: './register-account.component.html',
   styleUrls: ['./register-account.component.css']
 })
 export class RegisterAccountComponent implements OnInit {
+  user:User;
+  constructor(private router:Router) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.resetForm();
   }
 
+  onSubmit(){
+    this.router.navigate(['/personal']);
+  }
+  resetForm(form?:NgForm){
+    if(form!=null)
+    form.reset();
+    this.user = {
+      username:'',
+      password:'',
+      email_address:'',
+      first_name:'',
+      last_name:'',
+      type_of_account:'',
+      phone:'',
+    }
+  }
 }
