5ae2cfadfbdc0c12f1226954919f04972486d9d2
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-04-17 17:06:11 -0500

    added logic for the textfieldarea and logic to go back to account

--- frontend/src/app/components/business-invoices/business-invoices.component.ts
+++ frontend/src/app/components/business-invoices/business-invoices.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-business-invoices',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./business-invoices.component.css']
 })
 export class BusinessInvoicesComponent implements OnInit {
-
-  constructor() { }
+  note:string='';
+  maxLength:number =250;
+  characterCount:number =0;
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
-
+  updateCharacterCount(event:any){
+    this.characterCount = event.target.value.length;
+  }
+  backButton(){
+    this.router.navigate(['/business'])
+  }
 }
