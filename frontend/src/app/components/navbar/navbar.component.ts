a9b97e5a9635bcc01cd0ce95f4877cf177e88255
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-04-23 11:22:12 -0500

    updated navbar removed login register only visible on first load then gone after login

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
   }
-
+  get loggedIn() {
+    return this.authService.loggedIn;
+  }
 }
