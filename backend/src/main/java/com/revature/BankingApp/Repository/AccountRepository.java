31b3dfee72f0efb7d45642b4b7208d38a6a911e5
alex <ackerander@gmail.com>
2023-04-25 12:13:39 -0700

    Added find account features.

--- backend/src/main/java/com/revature/BankingApp/Repository/AccountRepository.java
+++ backend/src/main/java/com/revature/BankingApp/Repository/AccountRepository.java
@@ -2,7 +2,11 @@ package com.revature.BankingApp.Repository;
 
 import com.revature.BankingApp.Model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface AccountRepository<T extends Account> extends JpaRepository<T, Long> {
     T findByUsernameAndPasswd(String username, String passwd);
+    T findByUsername(String username);
+    List<T> findAllByEmail(String email);
+    List<T> findAllByPhone(String phone);
 }
