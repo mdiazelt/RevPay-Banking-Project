a6a9287abf0b55ca237349393c1faefd75eab55c
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-04-25 18:18:20 -0500

    able to comm with service to handle the post and validation of the forms

--- frontend/src/app/components/business-account/business-account.component.ts
+++ frontend/src/app/components/business-account/business-account.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {faFileInvoiceDollar} from '@fortawesome/free-solid-svg-icons';
-import {faFileCode} from '@fortawesome/free-regular-svg-icons';
-import {faLandmark} from '@fortawesome/free-solid-svg-icons';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { faFileInvoiceDollar } from '@fortawesome/free-solid-svg-icons';
+import { faFileCode } from '@fortawesome/free-regular-svg-icons';
+import { faLandmark } from '@fortawesome/free-solid-svg-icons';
+import { Card } from 'src/app/models/card';
+import { BusinessAddCreditCardService } from 'src/app/services/business-add-credit-card.service';
+import { User } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { formatDate } from '@angular/common';
 
 
 @Component({
@@ -11,23 +16,92 @@ import {faLandmark} from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./business-account.component.css']
 })
 export class BusinessAccountComponent implements OnInit {
+  cardForm = new FormGroup({
+    fullName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    number: new FormControl('', [Validators.required, Validators.maxLength(16)]),
+    cvc: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    expirationMonth: new FormControl('', [Validators.required]),
+    expirationYear: new FormControl('', [Validators.required])
+  });
+
+
   faFileInvoiceDollar = faFileInvoiceDollar;
-  faFileCode=faFileCode;
-  faLandmark=faLandmark;
-  constructor() {
+  faFileCode = faFileCode;
+  faLandmark = faLandmark;
 
-    
+
+  card: Card;
+  userId: number;
+
+
+  savedCards: Card[] = [];
+
+
+  constructor(private businessAddCreditCardService: BusinessAddCreditCardService, private authService: AuthService,private fb: FormBuilder) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
-  numbersOnly(event:any):boolean{
-    const charCode = (event.which)?event.which: event.keyCode;
-    if(charCode >31 && (charCode <48 || charCode > 57)){
+  saveCard(): void {
+
+
+    const userId = this.getUserId();
+    console.log('loggedInUserId:', userId);
+
+
+    if (userId) {
+      const expirationMonth = parseInt(this.cardForm.controls.expirationMonth.value || '');
+      const expirationYear = parseInt(this.cardForm.controls.expirationYear.value || '');
+      const expirationDate: Date = new Date(expirationYear, expirationMonth - 1, 1);
+      const card: Card = {
+        id: 0,
+        number: parseInt(this.cardForm.controls.number.value || '0'),
+        expiration: expirationDate,
+        cvc: parseInt(this.cardForm.controls.cvc.value || '0')
+      };
+
+      console.log('Form data:', this.cardForm.value);
+
+      this.businessAddCreditCardService.postCreditCard(card).subscribe({
+        next: (response) => {
+          console.log(response);
+          console.log('Credit card POST complete:', response);
+          console.log("Credit card information: ", card);
+        },
+        error: (error) => {
+          console.log(error);
+          console.log('Credit card POST failed:', error);
+        },
+      });
+    } else {
+      console.log('loggedIn:', this.authService.loggedIn);
+    }
+
+  }
+
+  numbersOnly(event: any): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
       console.log('charCode restricted is ' + charCode);
       return false;
     }
     return true;
   }
+  private getUserId(): number | null {
+    const userId = sessionStorage.getItem('userId') || (localStorage.getItem('userId'));
+    if (!userId) {
+      console.log('User ID not found');
+      return null;
+    }
+    return parseInt(userId);
+  }
 }
+
+
+    // const expirationDate: Date = new Date();
+    // expirationDate.setFullYear(
+    //   parseInt(this.cardForm.controls.expiration.value.year || '0'),
+    //   parseInt(this.cardForm.controls.expiration.value.month || '0')
+    // );
+
