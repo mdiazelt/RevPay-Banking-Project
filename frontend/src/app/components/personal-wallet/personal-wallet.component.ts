e812cd15b499c7dd3882e7255f62577e8b239b7f
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-04-27 11:28:01 -0500

    Added functionality to showcase credit info in the wallet page

--- frontend/src/app/components/personal-wallet/personal-wallet.component.ts
+++ frontend/src/app/components/personal-wallet/personal-wallet.component.ts
@@ -1,21 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Card } from 'src/app/models/card';
+import { DatePipe } from '@angular/common';
+
+import { PersonalAddCreditCardService } from 'src/app/services/personal-add-credit-card.service';
 @Component({
   selector: 'app-personal-wallet',
   templateUrl: './personal-wallet.component.html',
-  styleUrls: ['./personal-wallet.component.css']
+  styleUrls: ['./personal-wallet.component.css'],
+  providers: [DatePipe]
 })
 export class PersonalWalletComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private datePipe: DatePipe, private router:Router,private personalAddCreditCardService:PersonalAddCreditCardService) { }
 
   ngOnInit(): void {
+    this.personalAddCreditCardService.getCreditCard().subscribe(json => {
+      this.savedCards = json;
+      this.showCards = true;
+    });
   }
 
+  @Input() savedCards: Card[]=[];
+  showCards: boolean = false;
+
+
 back_button(){
-this.router.navigate(['/personal']);
+  console.log('Back button clicked');
+  this.router.navigateByUrl('/personal');
 }
 trans_button(){
   this.router.navigate(['/personal/transaction-history'])
 }
+
+// getCreditCardList():void{
+//  this.showCards = !this.showCards;
+//   this.savedCards = []; // Clear savedCards array
+//   this.personalAddCreditCardService.getCreditCard().subscribe(json => {
+//     this.savedCards = json;
+//   });
+// }
 }
+
